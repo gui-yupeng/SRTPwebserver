c77fb05bd4f4f9d498a99de8acb79c253d15a118
gui-yupeng <1413137503@qq.com>
2021-10-25 12:03:39 +0800

    将groupId写为aip_tools全局变量

--- aip_tools.js
+++ aip_tools.js
@@ -6,7 +6,8 @@ var API_KEY = "kdk7E4K3rz8AQSq9Qame4XaI";
 var SECRET_KEY = "EWsoztbfH2s2eQG2rEDDdbOGEkASfO9S";
 
 let client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
-
+// 测试项目，把groupId写死为'group_staff'
+let groupId='group_staff';
 //image取决于image_type参数，传入BASE64字符串或URL字符串或FACE_TOKEN字符串
 //var image ;
 //var imageType = "BASE64";
@@ -26,40 +27,57 @@ faceDetect= async function (image,imageType){
 
 addUser=async function(image,imageType, userId, user_info){
     let result=null;
-    //测试项目，把groupId写死为'group_staff'
-    groupId='group_staff';
+    
     let options = {};
     options["user_info"] = user_info;
-    // 带参数调用人脸更新
-    result=await client.addUser(image, imageType, groupId, userId, options);
+    if(client){
+        result=await client.addUser(image, imageType, groupId, userId, options);
+    }else{
+        client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
+        result=await client.addUser(image, imageType, groupId, userId, options);
+    }
+
     return result;
 }
 
 faceSearch= async function(image,imageType){
     //测试项目，把groupId写死为'group_staff'
-    let groupIdList = "group_staff";
+    let groupIdList = groupId;
     let result=null;
-    // 调用人脸搜索
-    result=await client.search(image, imageType, groupIdList);
+    if(client){
+        result=await client.search(image, imageType, groupIdList);
+    }else{
+        client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
+        result=await client.search(image, imageType, groupIdList);
+    }
+
     return result;
 }
 
 getUser = async function(userId){
-    //测试项目，把groupId写死为'group_staff'
-    let groupId = "group_staff";
     let result=null;
-    // 调用用户信息查询
-    result=await client.getUser(userId, groupId)
+    if(client){
+        result=await client.getUser(userId, groupId);
+
+    }else{
+        client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
+        result=await client.getUser(userId, groupId);
+    }
+    
+    return result;
 }
 
 getGroupUsers = async function(groupId){
-    //测试项目，把groupId写死为'group_staff'
-    let groupId = "group_staff";
     let result=null;
-    // 调用获取用户列表
-    result=await client.getGroupUsers(groupId)
+    if(client){
+        result=await client.getGroupUsers(groupId)
 
-
+    }else{
+        client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
+        result=await client.getGroupUsers(groupId)
+    }
+    
+    return result;
 }
 
 //注意，这些函数都是异步的
