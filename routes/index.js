1c79b53e239975057c99264d039d3666d9481952
gui-yupeng <1413137503@qq.com>
2021-10-25 15:29:50 +0800

    规范后端路由结构

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,17 @@
 exports.route = {
   //通过GET方法获取参数
   async get() {
+      let result={};
+      result.time=new Date().toLocaleString();
+      result.yourParams=this.params;
       
-      
-      return 'ok';
-  }
+      return result;
+  },
+  async post() {
+    let result={};
+    result.time=new Date().toLocaleString();
+    result.yourParams=this.params;
+    
+    return result;
+}
 };
