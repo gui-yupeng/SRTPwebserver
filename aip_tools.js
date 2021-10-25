c4510b5446dcad8767d3aec27b954b7402c4075f
gui-yupeng <1413137503@qq.com>
2021-10-25 11:49:00 +0800

    添加多个aip_tools内的封装接口

--- aip_tools.js
+++ aip_tools.js
@@ -11,7 +11,6 @@ let client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
 //var image ;
 //var imageType = "BASE64";
 
-// 调用人脸检测
 faceDetect= async function (image,imageType){
     let result=null;
     let options = {};
@@ -22,9 +21,6 @@ faceDetect= async function (image,imageType){
         client = new AipFaceClient(APP_ID, API_KEY, SECRET_KEY);
         result=await client.detect(image, imageType, options);
     }
-    
-
-    
     return result;
 }
 
@@ -34,14 +30,48 @@ addUser=async function(image,imageType, userId, user_info){
     groupId='group_staff';
     let options = {};
     options["user_info"] = user_info;
-
     // 带参数调用人脸更新
     result=await client.addUser(image, imageType, groupId, userId, options);
     return result;
 }
 
+faceSearch= async function(image,imageType){
+    //测试项目，把groupId写死为'group_staff'
+    let groupIdList = "group_staff";
+    let result=null;
+    // 调用人脸搜索
+    result=await client.search(image, imageType, groupIdList);
+    return result;
+}
+
+getUser = async function(userId){
+    //测试项目，把groupId写死为'group_staff'
+    let groupId = "group_staff";
+    let result=null;
+    // 调用用户信息查询
+    result=await client.getUser(userId, groupId)
+}
+
+getGroupUsers = async function(groupId){
+    //测试项目，把groupId写死为'group_staff'
+    let groupId = "group_staff";
+    let result=null;
+    // 调用获取用户列表
+    result=await client.getGroupUsers(groupId)
+
+
+}
+
 //注意，这些函数都是异步的
 module.exports={
+    // 人脸检测
     faceDetect,
-    addUser
+    // 添加用户
+    addUser,
+    // 人脸搜索
+    faceSearch,
+    // 用户信息查询
+    getUser,
+    // 获取用户组列表
+    getGroupUsers
 }
--- routes/faceSearch.js
+++ routes/faceSearch.js
@@ -1,17 +1,12 @@
-const addUser=require('../aip_tools').addUser;
+const faceSearch=require('../aip_tools').faceSearch;
 exports.route = {
   //通过POST方法获取参数
     async post() {
         let img=this.params.image;
-        let userId=this.params.userId;
-        let user_info=this.params.user_info
-
         //默认BASE64编码
         let imgType="BASE64";
-        let result=await addUser(img,imgType, userId, user_info);
-        
+        let result=await faceSearch(img,imgType);        
         //数据库操作
-
 
         return result;
     }
