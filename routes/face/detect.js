0ac16879537d52bfb71a13957655889739260da5
gui-yupeng <1413137503@qq.com>
2021-10-25 15:29:00 +0800

    规范后端路由结构

---
+++ routes/face/detect.js
@@ -0,0 +1,13 @@
+const faceDetect=require('../../aip_tools').faceDetect;
+exports.route = {
+  //通过POST方法获取参数
+  
+  async post() {
+    let img=this.params.image;
+    //console.log(img);不要再做类似的疯狂事了
+    //默认BASE64编码
+    let imgType="BASE64";
+    let result=await faceDetect(img,imgType);
+    return result;
+  }
+};
