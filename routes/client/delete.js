bb87e1c29ae3fc5b2599cffa5f87375d6afa2e54
gui-yupeng <1413137503@qq.com>
2021-11-08 22:56:25 +0800

    功能完善

--- routes/client/delete.js
+++ routes/client/delete.js
@@ -4,7 +4,17 @@ const dbMsg=require('../../dbMsg.json');
 exports.route = {
     
     async post() {
-        let result=await getGroup();
+        //默认只删除今天的
+        let user_id=this.params.id;
+        let msgCollection = await mongo(dbMsg.col_dayCheck);
+        let now=new Date();
+        let year=now.getFullYear();
+        let month=now.getMonth()+1;
+        let day=now.getDay(); 
+        let result=await msgCollection.deleteOne({"user_id":user_id,
+                                                    "year":year,
+                                                    "month":month,
+                                                    "day":day});
         return result;
     }
 };
